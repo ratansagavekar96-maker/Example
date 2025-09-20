abd837c70170ffed440f96c5cfd1b2c1693b5d57
Ratan <you@example.com>
2025-09-20 15:20:27 +0530

    HashSet code added

--- Example/src/test/ArrayListFilter.java
+++ Example/src/test/ArrayListFilter.java
@@ -1,12 +1,24 @@
 package test;
 
 import java.util.Arrays;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 public class ArrayListFilter {
 	
 		public static void main(String[] args) {
 			List<Integer> list =Arrays.asList(10,30,40,60,80);
-			list.stream().filter(p-> p%2==0).forEach(c->System.out.println(c));
+			//list.stream().filter(p-> p%2==0).forEach(c->System.out.println(c));
+			
+			Set set = new LinkedHashSet();
+			set.add(30);
+			set.add(40);
+			set.add(55);
+			set.add(67);
+			set.forEach(c->System.out.println(c));
+			
+			
+			
 			
 		
 	}
